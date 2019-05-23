dc9e26727b1b6b114270cfb20c7b485d5363a3c3
Ransom Byers <ransom.byers@gmail.com>
2019-05-22 20:24:53 -0700

    Add endpoint to serverless to test live deploy

--- spec/functions/heartland_interface_spec.rb
+++ spec/functions/heartland_interface_spec.rb
@@ -4,6 +4,10 @@ RSpec.describe HeartlandInterface do
   let(:hb) { HeartlandInterface.new }
 
   describe '#login' do
+    it 'returns true if successful' do
+
+    end
+
     it 'returns false if unsuccessful' do
       expect(hb.login).to be false
     end
